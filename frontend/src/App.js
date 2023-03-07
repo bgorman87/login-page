e425daf32ee99b06069962abb0b9542afe94ed0d
Brandon Gorman <bgorman@live.com>
2023-03-06 23:03:08 -0400

    removed unnecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
-import Header from "./components/Header.js";
+
 import PrivateRoutes from "./utils/PrivateRoutes";
 import { AuthProvider } from "./context/AuthContext.js";
+
 import "./styles/App.css"
 
 const App = () => {
